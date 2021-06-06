7bcfb1c3cc3e1b3dfa7c07f08b5afc2a95093d13
Muhammad Ayoub Khan <ayoubkhan558@hotmail.com>
2021-06-06 15:33:08 +0500

    init

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# web-platform-v9wpo6
+# Light & Dark Mode
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-v9wpo6)
+![screenshot](screenshot.png)
+
+The purpose of this project is to implement dark / light theme to a template website using plain JavaScript.
+
+<br>
+
+---
+
+## Learning Outcome
+
+<br>
+
+1. Using CSS variables
+2. `background: rgb(255 255 255 / 50%)` instead of `rgba(255, 255, 255, 0.5)`
+3. Creating a [toggle switch](https://www.w3schools.com/howto/howto_css_switch.asp)
+4. Setting attributes on the root element (html)
+5. Using `document.documentElement`
+6. Saving theme in localStorage
--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('hello!')
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+const DARK_THEME = "dark";
+const LIGHT_THEME = "light";
+
+// Dark or Light Images
+function imageMode(color) {
+	image1.src = `img/undraw_proud_coder_${color}.svg`;
+	image2.src = `img/undraw_feeling_proud_${color}.svg`;
+	image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+	if (event.target.checked) {
+		document.documentElement.setAttribute("data-theme", "dark");
+		localStorage.setItem("theme", "dark");
+	} else {
+		document.documentElement.setAttribute("data-theme", "light");
+		localStorage.setItem("theme", "light");
+	}
+}
+
+// Event Listeners
+toggleSwitch.addEventListener("change", switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+	document.documentElement.setAttribute("data-theme", currentTheme);
+
+	if (currentTheme === "dark") {
+		toggleSwitch.checked = true;
+	}
+}
